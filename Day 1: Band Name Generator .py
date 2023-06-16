ce5faf9fc35a1c34c6e031ed49bb7995d1ff0f35
Prince Vijay Santhosh Kongara <95931782+Prince-Vijay-Santhosh@users.noreply.github.com>
2023-06-16 17:25:10 +0530

    Create Day 1: Band Name Generator.py

---
+++ Day 1: Band Name Generator .py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+a = input("Enter the name of the city you grew up in: \n")
+b = input("Enter the name of your pet animal: \n")
+print("The name of your band is: " + a + " " + b)
